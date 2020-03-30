d188c7f36805c3a14bee3350e56353c3be766190
kkuop <60794860+kkuop@users.noreply.github.com>
2020-03-29 23:55:34 -0500

    started to add a front-end design with the searchByName function

--- app.js
+++ app.js
@@ -51,6 +51,26 @@ function mainMenu(person, people){
   }
   return mainMenu(person, people);
 }
+function searchByNameFE(people) {
+  closeAlert();
+  let firstName = document.getElementById("firstName").value;
+  let firstNameForSearch = firstName.toLowerCase();
+  let lastName = document.getElementById("lastName").value;
+  let lastNameForSearch = lastName.toLowerCase();
+
+  let foundPerson = people.filter(function(person) {
+    if(person.firstName.toLowerCase() === firstNameForSearch && person.lastName.toLowerCase() === lastNameForSearch){
+      return true;
+    } else {
+      return false;
+    }
+  });
+  if(foundPerson.length > 0) {
+      updatePage(foundPerson[0], people);
+    } else {
+      alert("No results found!");
+    }
+}
 function searchByName(people){
   let firstName = promptFor("What is the person's first name?", chars);
   let lastName = promptFor("What is the person's last name?", chars);
@@ -285,3 +305,79 @@ function convertedTraitName(critera)
   }
 }
 
+function closeAlert() {
+  collapseAll();
+  document.getElementById("alertSuccessByName").classList.add("do-not-display");
+  document.getElementById("alertSuccessByName").classList.remove("display");
+  document.getElementById("alertSuccessByName").innerHTML = "<button type='button' class='close' onclick='closeAlert()''>&times;</button><strong> found!</strong>"
+  document.getElementsByClassName("accordion")[0].classList.add("do-not-display");
+  document.getElementsByClassName("accordion")[0].classList.remove("display");
+}
+
+function collapseAll() {
+  let element = document.getElementsByClassName("card-body");
+  for (var i = 0; i < element.length; i++) {
+    element[i].classList.add("do-not-display");
+    element[i].classList.remove("display");
+  };
+}
+
+function openInfo() {
+
+  collapseAll();
+  document.getElementById("infoHere").classList.add("display");
+  document.getElementById("infoHere").classList.remove("do-not-display");
+}
+
+function openFamily() {
+  collapseAll();
+  document.getElementById("familyHere").classList.add("display");
+  document.getElementById("familyHere").classList.remove("do-not-display");
+}
+
+function openDescendants() {
+  collapseAll();
+  document.getElementById("descendantsHere").classList.add("display");
+  document.getElementById("descendantsHere").classList.remove("do-not-display");
+}
+
+function updatePage(person, people) {
+    let alertSuccessByName = document.getElementById("alertSuccessByName");
+    alertSuccessByName.classList.add("display");
+    alertSuccessByName.classList.remove("do-not-display");
+    alertSuccessByName.innerHTML = "<strong>" + person.firstName + " " + person.lastName + "</strong>" + alertSuccessByName.innerHTML;
+    document.getElementsByClassName("accordion")[0].classList.remove("do-not-display");
+    document.getElementById("infoHere").innerHTML = 
+      "First Name: " + person.firstName + 
+      "<br>Last Name: " + person.lastName +
+      "<br>Gender: " + person.gender +
+      "<br>Date of Birth: " + person.dob +
+      "<br>Height: " + person.height + 
+      "<br>Weight: " + person.weight +
+      "<br>Eye Color: " + person.eyeColor;
+    let familyString = "";
+    let spouse = findSpouse(person, people);
+    let children = findChildren(person, people);
+    let parents = findParents(person, people);
+    familyString += `Spouse: ${spouse.firstName} ${spouse.lastName}<br>`
+    familyString += `${parents.map(function(person) {
+        return "Parent: " + person.firstName + " " + person.lastName;
+      }).join("<br>")}`;
+    familyString += `${children.map(function(person) {
+        return "Child: " + person.firstName + " " + person.lastName;
+      }).join("<br>")}`;
+    document.getElementById("familyHere").innerHTML = familyString;
+    let descendantsString = "";
+    let descendants = findDescendants(person, people);
+    if(descendants.length > 0) {
+      descendantsString += `${descendants.map(function(person) {
+      return "Name: " + person.firstName + " " + person.lastName;
+    }).join("<br>")}`;
+    } else {
+      descendantsString += `No descendants found!`;
+    }
+    
+    document.getElementById("descendantsHere").innerHTML = descendantsString;
+    document.getElementById("firstName").value ="";
+    document.getElementById("lastName").value ="";
+}
